9a5a717fbde272ecfc25e53e2368a752dc6ffb9c
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-05-19 20:18:19 +0530

    Modify the keynote management functions

--- src/plugins/api/modules/keynote/Keynote.controller.js
+++ src/plugins/api/modules/keynote/Keynote.controller.js
@@ -1,27 +1,32 @@
 import Keynote from './Keynote.model';
 import response from '../../../../lib/response.handler';
+import _ from 'lodash';
 
-//create new keynote - core
+//create new keynote
 export async function createKeynote (req, res, next){
   if (req.body) {
-    let keynote = new Keynote(req.body);
-    await keynote.save()
-    .then(() => {
-      response.sendRespond(res, keynote);
-      next();
-    })
-    .catch(error => {
-      response.handleError(res, error.message);
-      next();
-    });
+    if (_.isEqual(req.user.role, 'ROLE_EDITOR')) {
+      let keynote = new Keynote(req.body);
+      await keynote.save()
+      .then(() => {
+        response.sendRespond(res, keynote);
+        next();
+      })
+      .catch(error => {
+        response.handleError(res, error.message);
+        next();
+      });
+    } else {
+      response.handleError(res, 'Only editor can create a keynote');
+      return;
+    }
   }
 }
 
 //get all keynote details
 export async function getAllKeynotes(req, res, next) {
   await Keynote.find({})
-  .populate('', '')
-  .populate('', '')
+  .populate({ path: 'resource', populate: { path: 'resourcepersons', model: 'users', select: 'firstname lastname email phonenumber description imageurl'}})
   .then((data) => {
     response.sendRespond(res, data);
     next();
@@ -34,36 +39,114 @@ export async function getAllKeynotes(req, res, next) {
 
 //get keynote details - core
 export function getKeynoteById(req, res, next) {
-  if (req.keynote) {
-    response.sendRespond(res, req.keynote);
-    next();
+  if (req.params && req.params.id) {
+    await Keynote.findById(req.params.id)
+    .populate({ path: 'resource', populate: { path: 'resourcepersons', model: 'users', select: 'firstname lastname email phonenumber description imageurl'}})
+    .then((data) => {
+      response.sendRespond(res, data);
+      next();
+    })
+    .catch(error => {
+      response.handleError(res, error.message);
+      next();
+    });
   } else {
-    response.sendNotFound(res);
+    response.sendNotFound(res, 'Key note id not found');
+    return;
   }
 }
 
 // update user - core
 export async function updateKeynote(req, res, next) {
   if (req.body) {
-    let updateData = {
-      title: req.body.title,
-      keynoteimageurl: req.body.keynoteimageurl,
-      conforenceid: req.body.conforenceid
-    };
-    const keynote = await keynote.findByIdAndUpdate(req.user.id, updateData);
-    response.sendRespond(res, keynote);
-    next();
+    if (_.isEqual(req.user.role, 'ROLE_EDITOR')) {
+      let updateData = {
+        title: req.body.title,
+        keynoteimageurl: req.body.keynoteimageurl,
+        description: req.body.description
+      };
+      const keynote = await keynote.findByIdAndUpdate(req.user._id, updateData);
+      response.sendRespond(res, keynote);
+      next();
+    } else {
+      response.handleError(res, 'Only editor can create a keynote');
+      return;
+    }
   } else {
     response.handleError(res, 'Please provide necessary fields');
+    return;
+  }
+}
+
+export async function changeKeynoteStatus(req, res, next) {
+  if (req.user && req.params && req.params.id) {
+    if (_.isEqual(req.user.role, 'ROLE_ADMIN')) {
+      let status = null;
+      let keynote = await Keynote.findById(req.params.id);
+      if (!keynote) {
+        response.handleError(res, 'Keynote not found');
+        return;
+      }
+
+      if (_.isEqual(req.body.status, 'APPROVED')) {
+        // mark keynote as approved
+        status = 'APPROVED';
+
+        // send a notification to relevent user
+        let notificationData = {
+          keynote: keynote._id,
+          from: req.user._id,
+          message: `Your keynote is approved by ${req.user.firstname}`,
+          to: keynote.createdby,
+          isarchive: false
+        }
+        let notification = new Notification(notificationData);
+        await notification.save()
+      }
+
+      if (_.isEqual(req.body.status, 'PENDING')) {
+        // mark keynote as pending
+        status = 'PENDING';
+
+        // send a notification to relevent user
+        let notificationData = {
+          keynote: keynote._id,
+          from: req.user._id,
+          message: `Your resources are rejected by ${req.user.firstname}`,
+          to: keynote.createdby,
+          isarchive: false
+        }
+        let notification = new Notification(notificationData);
+        await notification.save()
+      }
+
+      await Keynote.findByIdAndUpdate(req.params.id, { status: status })
+      .then(data => {
+        response.sendRespond(res, data);
+        next();
+      })
+      .catch(error => {
+        response.handleError(res, error.message);
+        next();
+      });
+    } else {
+      response.handleError(res, 'Only Admin can change the status');
+      next();
+    }
   }
 }
 
 // delete keynote - core
 export async function deleteKeynote(req, res, next) {
   if (req.params && req.params.id) {
-    const keynote = await Keynote.findIdAndDelete(req.keynote.id);
-    response.sendRespond(res, keynote);
-    next();
+    if (_.isEqual(req.user.role, 'ROLE_EDITOR')) {
+      const keynote = await Keynote.findIdAndDelete(req.keynote.id);
+      response.sendRespond(res, keynote);
+      next();
+    } else {
+      response.handleError(res, 'Only Editor can delete the keynote');
+      next();
+    }
   } else {
     response.handleError(req, 'Parameter id is required');
     next();
--- src/plugins/api/modules/keynote/Keynote.model.js
+++ src/plugins/api/modules/keynote/Keynote.model.js
@@ -1,10 +1,12 @@
 import mongoose, { Schema } from 'mongoose';
 
 const KeynoteSchema = new Schema({
-    title : { type: String, required: [true, 'Title should be provided'], trim: true },
+    title : { type: String, required: [false, 'Title should be provided'], trim: true },
     description: {  type: String, required: [true, 'Title should be provided'], trim: true },
     keynoteimageurl : { type: String, required: false, trim: true },
-    resouces: [{type:Schema.Types.ObjectId, required:true, ref:'resources'}]
+    createdby: { type: Schema.Types.ObjectId, required: true, ref: 'users' },
+    resouce: { type:Schema.Types.ObjectId, required:true, ref:'resources' },
+    status: { type: String, required: true, trim: false }
 },
     {strict: false},
     {timestamps : true}
