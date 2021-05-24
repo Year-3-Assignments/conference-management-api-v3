182efb9d26729df59db1fd2bcd17a77da6fa1be6
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-05-25 01:12:05 +0530

    Modify the chargeAmount function

--- src/plugins/api/modules/payment/Payment.controller.js
+++ src/plugins/api/modules/payment/Payment.controller.js
@@ -5,67 +5,67 @@ import User from '../user/User.model.js'
 import Conference from '../conference/Conference.model'
 import Payment from '../payment/Payment.model'
 
-export async function changeAmount(req, res, next) {
-
+export function chargeAmount(req, res, next) {
   if (req.user && req.body) {
-
-    try {
-      // let { status } = await stripe.charges.create({
-      //   amount: req.body.amount,
-      //   currency: 'LKR',
-      //   source: req.body.token
-      // })
-
-      // Add Payment Details to Payment Collection
+    return new Promise((resolve, reject) => {
       let paymentDetail ={
         conference: req.body.conference_id,
         attendee: req.user._id,
         amount: '200',
         source: "Testing Source"
-      }
-
-      let conference_name = ""
+      };
 
       let payment = new Payment(paymentDetail);
-      await payment.save()
-      .then(async () => {
-        let data = await Conference.findById(req.body.conference_id);
-        conference_name = data.name;
-      })
-      .then(async () =>{
-
-        console.log(conference_name);
-
-        let notificationDetail ={
-          conference: req.body.conference_id,
-          message: "Your payment has been successful and now you can attend the Conference "+conference_name,
-          to: req.user._id,
-          isarchive: false
+      payment.save()
+      .then(paymentInformation => {
+        if (paymentInformation) {
+          let conference = Conference.findById(req.body.conference_id);
+          if (conference) {
+            let notificationData ={
+              conference: req.body.conference_id,
+              message: "Your payment has been successful and now you can attend the Conference "+ conference.name,
+              to: req.user._id,
+              isarchive: false
+            };
+            let notification = new Notification(notificationData);
+            notification.save()
+            .then(notificationInformation => {
+              if (notificationInformation) {
+                let updateConference = Conference.findByIdAndUpdate({ _id: req.body.conference_id }, { $push: {atendees: req.user._id}});
+                if (updateConference) {
+                  let updateUser = User.findByIdAndUpdate({ _id: req.user._id }, { $push: { attending_conferences: req.body.conference_id }});
+                  if (updateUser) {
+                    let responseData = {
+                      payment: notification,
+                      notification: notification,
+                      user: updateUser,
+                      conference: conference
+                    }
+                    return resolve(notificationInformation);
+                  } else {
+                    return resolve(JSON.stringify(paymentInformation));
+                  }
+                } else {
+                  return resolve({ paymentInformation, notificationInformation });
+                }
+              } else {
+                return resolve({ paymentInformation });
+              }
+            })
+            .catch(error => response.handleError(res, error.message));
+          }
+        } else {
+          return resolve({ paymentInformation });
         }
-
-        let notification = new Notification(notificationDetail);
-        await notification.save()
-
       })
-      .then(async() => {
-        await Conference.findByIdAndUpdate({ _id: req.body.conference_id }, { $push: {atendees: req.user._id}})
-      })
-      .then(async() => {
-        await User.findByIdAndUpdate({ _id: req.user._id }, { $push: { attending_conferences: req.body.conference_id }})
-      })
-      .then((data) => {
-        response.sendRespond(res, data);
-        return;
-      })
-      .catch(error => {
-        // response.handleError(res, error.message);
-        return;
-      });
-
-    } catch (error) {
-      response.handleError(res, error.message);
-      return;
-    }
-
+    })
+    .then(paymentResponse => { 
+      console.log(paymentResponse)
+      response.sendRespond(res, paymentResponse)
+    })
+    .catch(error => { 
+      console.log(error)
+      response.handleError(res, error.message)
+    });
   }
 }
