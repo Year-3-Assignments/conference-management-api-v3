ee13610e7e7918f3a69c96eb0359f0fc51414f31
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-05-24 23:59:25 +0530

    Add modifications to confercen controller

--- src/plugins/api/modules/user/User.controller.js
+++ src/plugins/api/modules/user/User.controller.js
@@ -175,38 +175,12 @@ export async function getEditorAccounts(req,res, next) {
   }
 }
 
-// get all researcher accounts | private | admin only
-export async function getResearcherAccounts(req, res, next) {
+// get all user accounts | private | admin only
+export async function getAllUserAccounts(req, res, next) {
   if (_.isEqual(req.user.role, 'ROLE_ADMIN')) {
-    const researcherAccounts = await User.find({ role: 'ROLE_RESEARCHER' });
+    const researcherAccounts = await User.find({ role: 'ROLE_USER' });
     if (researcherAccounts) {
       response.sendRespond(res, researcherAccounts);
-      next();
-    } else {
-      response.sendNotFound(res);
-    }
-  }
-}
-
-// get all presenter accounts | private | admin only
-export async function getPresenterAccounts(req, res, next) {
-  if (_.isEqual(req.user.role, 'ROLE_ADMIN')) {
-    const presenterAccounts = await User.find({ role: 'ROLE_PRESENTER' });
-    if (presenterAccounts) {
-      response.sendRespond(res, presenterAccounts);
-      next();
-    } else {
-      response.sendNotFound(res);
-    }
-  }
-}
-
-// get all atendee accounts | private | admin only
-export async function getAtendeeAccounts(req, res, next) {
-  if (_.isEqual(req.user.role, 'ROLE_ADMIN')) {
-    const atendeeAccounts = await User.find({ role: 'ROLE_ATENDEE' });
-    if (atendeeAccounts) {
-      response.sendRespond(res, atendeeAccounts);
       next();
     } else {
       response.sendNotFound(res);
