88dd22585dbe7b85e8b91ce13a1fcede79c7150d
parathantl <parathanlive123@gmail.com>
2021-06-29 22:24:00 +0530

    Made changes in Modal and functions

--- src/plugins/api/modules/resource/Resource.model.js
+++ src/plugins/api/modules/resource/Resource.model.js
@@ -13,8 +13,6 @@ const ResourceSchema = new Schema({
   resourceurls: [{ type: String, required: true, trim: true }],
   ispaid: { type: Boolean, required: false, default: false },
   resourcepersons: [{ type: Schema.Types.ObjectId, required: true, ref: 'users' }],
-  isEditor: { type: Boolean, required: false, default: false },
-  isAdminApproved: { type: Boolean, required: false, default: false },
 }, {
   timestamps: true
 });
